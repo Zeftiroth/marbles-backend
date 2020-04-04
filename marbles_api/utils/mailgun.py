4a993e8333c35a827325219c1d84e3e225ca23ee
Zeftiroth <zeft.huisen@gmail.com>
2020-04-04 23:17:54 +0800

    bugfix mailgun

--- marbles_api/utils/mailgun.py
+++ marbles_api/utils/mailgun.py
@@ -4,7 +4,7 @@ import requests
 
 def send_simple_message(cUser, eConName, eConEmail, eConRelation):
 
-    return requests.post("https://api.mailgun.net/v3/sandboxa290eb457da745fdad8b11f801694050.mailgun.org/   messages", auth=("api", "d1bd19e4a381b3a48371b54f3d02a719-c322068c-af9fe518"),
+    return requests.post("https://api.mailgun.net/v3/sandboxa290eb457da745fdad8b11f801694050.mailgun.org/   messages", auth=("api", os.getenv("MAILGUN_API_KEY")),
                          data={"from": "Marbles App <mailgun@sandboxa290eb457da745fdad8b11f801694050.mailgun.org >",
                                "to": [{eConEmail}],
                                "subject": f"{cUser} has pressed the panic button!",
